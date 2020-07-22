c761f33e2c61ba3ecf71f5dde5fa6196132330ed
Ankit Nahar <M1055071@mindtree.com>
2020-07-22 12:09:20 +0530

    first commit

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3001
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    res.end('<h1>Hello World from node</h1>');
 });
 server.listen(port, () => {
     console.log(`Server running at port ` + port);
